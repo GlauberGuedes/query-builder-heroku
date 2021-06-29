7705da5174e057fd550a507bc306f3d327c64228
glauberguedes <glauber-94@hotmail.com>
2021-06-29 16:46:23 -0300

    deploy heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,13 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'market_cubos'
+        host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
+        user: 'dhoieafqbyegsw',
+        password: '4c85fdf3c25e1a567974f54d090106c2e51e89c6c910404851ff43de6d19954e',
+        database: 'd3nlm6737c97vb',
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
